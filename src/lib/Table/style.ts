3788b8f1312812d9f9d3f2d554a97b7e50d83c6b
inyeong-kang <tomatorkd@naver.com>
2023-10-11 17:44:59 +0900

    feat: (#5) Table 컴포넌트 구현, 스토리 작성

---
+++ src/lib/Table/style.ts
@@ -0,0 +1,76 @@
+import { styled } from 'styled-components';
+
+export const Wrapper = styled.section`
+  @media (max-width: 679px) {
+    overflow-x: scroll;
+  }
+`;
+
+export const Table = styled.table`
+  width: 100%;
+  min-width: 270px;
+  min-height: 80px;
+
+  font: var(--text-caption);
+  text-align: center;
+
+  border: 1px solid #e2e2e2;
+
+  @media (max-width: 679px) {
+    overflow-x: scroll;
+    border-collapse: collapse;
+  }
+`;
+
+export const Tbody = styled.tbody`
+  width: 200px;
+
+  & > :nth-child(11) {
+    margin-top: 20px;
+    padding: 3px 0;
+    border-radius: 4px;
+
+    background-color: var(--white);
+
+    font-weight: 600;
+  }
+`;
+
+export const THead = styled.thead`
+  background-color: #dcf0fa;
+`;
+
+export const Tr = styled.tr<{ $childLength: number }>`
+  display: grid;
+  grid-template-columns: ${(props) => `repeat(${props.$childLength}, 1fr)`};
+  grid-gap: 10px;
+
+  width: 100%;
+  border-bottom: 1px solid #e2e2e2;
+  padding: 0 15px;
+
+  &:hover {
+    background-color: #deeef6;
+  }
+`;
+
+export const Th = styled.th`
+  padding: 10px 0;
+
+  font: var(--text-caption);
+  font-weight: 600;
+`;
+
+export const Td = styled.td<{ $dense: boolean }>`
+  padding: ${(props) => (props.$dense ? '5px 0' : '10px 0')};
+`;
+
+export const EmptyData = styled.p`
+  margin: 15px 0;
+`;
+
+export const StyledButton = styled.button`
+  border: 1px solid gray;
+  border-radius: 5px;
+  padding: 5px;
+`;
--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -2,5 +2,6 @@ export { default as LoadingSpinner } from './LoadingSpinner';
 export { default as SquareButton } from './SquareButton';
 export { default as Skeleton } from './Skeleton';
 export { default as ToggleSwitch } from './ToggleSwitch';
+export { default as Table } from './Table';
 export { default as RoundButton } from './RoundButton';
 export { default as VDSProvider } from './VDSProvider';

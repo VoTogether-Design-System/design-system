4d3782298bd6e2341b1b43d061334f5b5ede7f1c
inyeong-kang <tomatorkd@naver.com>
2023-10-11 15:10:02 +0900

    chore: 컴포넌트 모듈 경로 추가

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,2 +1,6 @@
+export { default as LoadingSpinner } from './LoadingSpinner';
+export { default as SquareButton } from './SquareButton';
+export { default as Skeleton } from './Skeleton';
+export { default as ToggleSwitch } from './ToggleSwitch';
 export { default as RoundButton } from './RoundButton';
 export { default as VDSProvider } from './VDSProvider';

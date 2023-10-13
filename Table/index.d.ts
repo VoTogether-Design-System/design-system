ac424eb3b4ead1c91dde0e5fda74dc45b6259c8d
inyeong-kang <inyeong-kang@users.noreply.github.com>
2023-10-13 07:23:49 +0000

    deploy: 08978490dc3f0abc6e3d055c2118f753c862fccb

--- Table/index.d.ts
+++ Table/index.d.ts
@@ -2,6 +2,7 @@ interface TableProps<T> {
     columns: string[];
     rows: T;
     dense?: boolean;
+    columnTemplate?: string;
 }
-export default function Table<T extends object>({ columns, rows, dense, }: TableProps<T[]>): import("react/jsx-runtime").JSX.Element;
+export default function Table<T extends object>({ columns, rows, dense, columnTemplate, }: TableProps<T[]>): import("react/jsx-runtime").JSX.Element;
 export {};

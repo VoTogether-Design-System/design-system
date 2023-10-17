eed10bc136b3e3378f69f4a1af9727bc987d2128
Gilpop8663 <Gilpop8663@users.noreply.github.com>
2023-10-17 04:15:16 +0000

    deploy: 2a38ff84f05f392fe0c81f7496650015309a56c1

--- assets/preview-861bbaec.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-da5d8c0e.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-5107f675.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-5107f675.js","./iframe-da5d8c0e.js","./index-c013ead5.js","./_commonjsHelpers-725317a4.js","./_getPrototype-dd8f0158.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-861bbaec.js.map
--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,1 @@
 export type Size = 'sm' | 'md' | 'lg';
-export type StringPercent = `${number}px`;
-export type StringPixel = `${number}px` | `${number}px ${number}px` | `${number}px ${number}px ${number}px` | `${number}px ${number}px ${number}px ${number}px`;
